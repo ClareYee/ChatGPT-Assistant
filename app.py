0e3ed4fa6e2cb3ec1cf0b540e1d2d89b7d399702
ClareYee <99359320+ClareYee@users.noreply.github.com>
2023-10-13 01:24:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ with st.sidebar:
     st.write("\n")
     st.text_input("设定会话名称：", key="set_chat_name", placeholder="输入会话名")
     st.selectbox(
-        "AI模型：", index=0, options=["gpt-3.5-turbo"], key="select_model"
+        "AI模型：", index=0, options=["PooshAI"], key="select_model"
     )
     st.write("\n")
     st.caption(
